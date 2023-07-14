d6c9d01109406c8e42336242544e93883eb0b0ef
Neel Sattigiri <57190894+neelsattigiri@users.noreply.github.com>
2023-07-14 19:11:30 +0530

    Add files via upload
    updated comments

--- EthernetCommunication/Ethernet_Client.py
+++ EthernetCommunication/Ethernet_Client.py
@@ -9,26 +9,26 @@ import canBUS
 host = 'local host'
 port = 5000
 
-# create a socket at client side
-# using TCP / IP protocol
+# create a socket for client
 s = socket.socket(socket.AF_INET,
                   socket.SOCK_STREAM)
 
-# connect it to server and port
-# number on local computer.
+# connect socket to a port on local machine
 s.connect(('127.0.0.3', port))
 
-# receive message string from
-# server, at a time 1024 B
+# receive message as a string from the server
 msg = s.recv(1024)
 
-# repeat as long as message
-# string are not empty
+# loop till message is not received
+
 while msg:
 
     msg = s.recv(1024)
+    # try to load the data using pickle
     try:
         msg = pickle.loads(msg)
+        # a buffer is used between ethernet and CAN
+        # buffer is used to convert to ASCII if input is a char
         buffer = []
         for i in range(1, len(msg)):
             if isinstance(msg[i], str):
@@ -37,12 +37,14 @@ while msg:
                 temp = msg[i]
             buffer.append(temp)
 
+        # Separate into 2 CAN messages based on ID at first index
         if msg[0] == "VehicleParams":
             Rx_VehicleSpeed = buffer[0]
             Rx_VehicleAngle = buffer[1]
             Rx_VehicleType = buffer[2]
             msg1 = can.Message(
                 arbitration_id=0xabcde, data=buffer)
+        # different arbitration id used to separate CAN messages
         elif msg[0] == "internalParams":
             Rx_AccelPedlPos = buffer[0]
             Rx_Transmission = buffer[1]
@@ -52,10 +54,14 @@ while msg:
             msg1 = can.Message(
                 arbitration_id=0xabcdf, data=buffer)
 
+        # send data to can bus
         canBUS.bus1.send(msg1)
-        msg2 = canBUS.bus2.recv()
-#        canBuffer = [msg2.data[0]]
 
+        # receive data from can bus
+        msg2 = canBUS.bus2.recv()
+
+        # identify message using arbitration_id - acts like a CANdb
+        # Plan to implement as a separate library
         if msg2.arbitration_id == 0xabcde:
             print('Speed=', msg2.data[0])
             print('Angle=', msg2.data[1])
@@ -72,7 +78,7 @@ while msg:
             print("---------------------------------------------------------") 
 
 
-
+    # print exception message if an exception occurs
     except:
         print("exception")
 
--- EthernetCommunication/canBUS.py
+++ EthernetCommunication/canBUS.py
@@ -1,4 +1,4 @@
 import can
-
+# initialise can bus 1 and 2
 bus1 = can.interface.Bus('test', bustype='virtual')
 bus2 = can.interface.Bus('test', bustype='virtual')
